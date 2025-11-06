912b03f6ae4b1d05590d5e4b93b69270ff47c6e6
sp4tn1c <152334741+sp4tn1c@users.noreply.github.com>
2025-11-06 19:00:11 +0300

    Create ThreeDigitsSum.cpp

---
+++ ThreeDigitsSum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    for (int i = 0; i <= 4; i += 2) {
+
+    }
+
+    // Вычисление суммы цифр
+    int sum = digit1 + digit2 + digit3;
+
+    // Вывод результата
+    cout << "Число: " << number << endl;
+    cout << "Цифры: " << digit1 << ", " << digit2 << ", " << digit3 << endl;
+    cout << "Сумма цифр: " << sum << endl;
+
+}
